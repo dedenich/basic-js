22e63f467cb2b7576f2c2ed09282fe80dd21d72f
Dedenich <sergbrst@tut.by>
2020-03-02 14:26:03 +0300

    add createDreamTeam

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,14 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+
+  let teamName = []
+
+  for (let i = 0; i < members.length; i ++){
+    if (typeof members[i] === "string"){
+      let init = members[i].match(/\w/i)[0];
+      teamName.push(init.toUpperCase());
+    }
+  }
+
+  return teamName.sort().join("");
 };
