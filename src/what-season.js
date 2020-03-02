bec94f824b305d2de791ce3dfd31865b0ceaa8ab
Dedenich <sergbrst@tut.by>
2020-03-02 14:42:22 +0300

    add getSeason

--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,23 @@
-module.exports = function getSeason(/* date */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (arguments.length === 0) return 'Unable to determine the time of year!';
+  if (Object.prototype.hasOwnProperty.call(date, 'getMonth')) throw Error;
+
+  let monthN;
+
+  try {
+    monthN = date.getMonth();
+  } catch (error) {
+    throw error;
+  }
+
+  if (monthN < 2 || monthN > 10)
+    return "winter";
+
+  if (monthN > 1 && monthN < 5)  
+    return "spring";
+
+  if (monthN > 4 && monthN < 8) 
+    return "summer";
+  if (monthN > 7 && monthN < 11)  
+   return "autumn";
 };
