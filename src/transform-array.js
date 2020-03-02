e5ca282d9985aeb135a2acc03a5628afa341d779
Dedenich <sergbrst@tut.by>
2020-03-02 18:24:32 +0300

    add transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr)) throw Error;
+    if (arr.length === 0) return arr;
+    let resArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (!/^--/.test(arr[i])) {
+            resArr.push(arr[i]);
+        } else {
+            switch (arr[i]) {
+                case "--discard-next":
+                    i++;
+                    break;
+                case "--discard-prev":
+                    if(resArr.length > 0) resArr.pop();
+                    break;
+                case "--double-next":
+                    if (arr.length > i + 1) resArr.push(arr[i + 1]);
+                    break;
+                case "--double-prev":
+                    if(i >= 1) resArr.push(arr[i - 1]);
+                    break;
+            }
+        }
+    }
+    return resArr;
 };
