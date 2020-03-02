83c080a9a75a5f24784631c6a5277e6f9c0f0fde
Dedenich <sergbrst@tut.by>
2020-03-02 14:11:21 +0300

    add carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
+const LN2 = 0.693;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(str) {
+  if (typeof str !== "string") return false;
+
+  let parsedStr = parseFloat(str);
+  if (parsedStr > 0 && parsedStr < MODERN_ACTIVITY) {
+    let year = Math.log(MODERN_ACTIVITY / parsedStr) / (LN2 / HALF_LIFE_PERIOD);
+    
+    return Math.ceil(year);
+  } 
+  return false;
 };
