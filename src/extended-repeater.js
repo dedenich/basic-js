2295e3f745643bbcaa62241d72fbfb74effd07cb
Dedenich <sergbrst@tut.by>
2020-03-02 15:02:36 +0300

    add repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,19 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    
+        let additionStr;
+        
+        if (options.addition !== undefined) {
+            let additionRepeatTimes = options.additionRepeatTimes || 1;
+            let additionArr = Array(additionRepeatTimes).fill(String(options.addition));
+            let additionSeparator = options.additionSeparator || '|';
+            additionStr = additionArr.join(additionSeparator);
+        }
+    
+        let repeatTimes = options.repeatTimes || 1;
+        let fillArr = additionStr ? String(str) + additionStr : String(str);
+        let arr = Array(repeatTimes).fill(fillArr);
+        let separator = options.separator || '+';
+    
+        return arr.join(separator);
 };
   
